752f6fc97a95029cf511b272c92f2656d466a7dc
anaYesi <anayesenia026@gmail.com>
2017-07-18 15:06:29 -0500

    modifique el ejercicio 1

--- ejercicio3.js
+++ ejercicio3.js
@@ -4,6 +4,7 @@ function convertirCelsius()
 {
 	var f = parseInt(document.getElementById('ingreso').value);
 	var c =(f-32)+5/9;
+	//Math.trunc() quita decimales pero no redondea
 	document.getElementById("respuesta").innerHTML = Math.trunc(c);
 	
 }
